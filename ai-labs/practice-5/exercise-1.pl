b471290a733e48e4202d90bfb02ee0f68f04fb3c
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-05-05 18:10:13 +0200

    Add exercise 1 for practice 5

---
+++ ai-labs/practice-5/exercise-1.pl
@@ -0,0 +1,28 @@
+/*
+
+Exercise 1:
+
+Everyone who teaches Computer Science subjects is smart.
+Juan teaches Algorithmics subject.
+Juan's wife teaches AI subject.
+Algorithmics is a subject of Maths.
+AI is a subject of CS.
+Is there anyone smart?
+
+Domain: Universe
+
+Functions:
+  smart(X) -> X is smart
+  teach(X, Y) -> X teaches Y
+  belongTo(X, Y) -> X belongs to Y
+
+Question:
+  smart(_).
+
+*/
+
+smart(X) :- teach(X, Y), belongTo(Y, computerScience).
+teach(juan, algorithmics).
+teach(juanWife, artificialIntelligence).
+belongTo(algorithmics, computerScience).
+belongTo(artificialIntelligence, computerScience).
