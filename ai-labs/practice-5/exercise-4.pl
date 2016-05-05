8ab290f36d814c47f9aaed46c1d93718859c3d7c
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-05-05 20:08:27 +0200

    Add exercise 4 for practice 5

---
+++ ai-labs/practice-5/exercise-4.pl
@@ -0,0 +1,38 @@
+/*
+
+Exercise 4:
+
+Juan likes all type of food.
+Apples are food.
+Chicken is food.
+Whatever you eat and you don't kill is food.
+Guillermo eats peanuts and he's alive.
+Susana eats all what Guillermo eats.
+Does Juan likes peanuts?
+
+Domain: Universe
+
+Functions:
+  like(X, Y) -> X likes Y
+  isKindOf(X, Y) -> X is a kind of Y
+  eat(X, Y) -> X eats Y
+  kill(X, Y) -> X kills Y
+  isAlive(X) -> X is alive
+
+Query:
+  like(juan, peanuts)
+
+Solution:
+  false.
+
+Write code bellow this
+
+*/
+
+like(juan, food).
+isKindOf(apple, food).
+isKindOf(chicken, food).
+isKindOf(X, food) :- not kill(Y, X), eat(Y, X).
+eat(guillermo, peanuts).
+isAlive(guillermo).
+eat(susana, X) :- eat(guillermo, X).
