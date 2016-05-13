53b96064e24107b85ec5aadb61a617746cd45ccf
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-05-13 21:05:48 +0200

    Add exercise 3 for practice 7

---
+++ ai-labs/practice-7/exercise-3.pl
@@ -0,0 +1,18 @@
+/*
+
+Exercise 3:
+Justify the answers to these queries in Prolog engine.
+
+(a)?- 5 + 2 is 3 + 4.
+false. "is" doesn't make operations in both members to evaluate.
+
+(b)?- 5 + 2 = 3 + 4.
+false. "=" doesn't make operations in both members to evaluate.
+
+(c) ?- 5 + 2 =:= 3 + 4.
+true. "=:=" forces to Prolog to solve operations in members.
+
+(d) ?- 5 + 2 \= 3 + 4.
+true. "\=" just searches the unification.
+
+*/
